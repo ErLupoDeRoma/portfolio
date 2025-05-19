7a96a29d096f7fdd05648c3086875ac5bbb433f1
ErLupoDeRoma <l.rivoli@donboscosandona.it>
2025-05-19 16:35:37 +0200

    Cambio dello stile dell elemento Riquadro_Navigazione

--- portfolio/components/Riquadro_Navigazione.js
+++ portfolio/components/Riquadro_Navigazione.js
@@ -1,16 +1,20 @@
 export default function Navigazione() {
   return (
     <>
-      <div id="generale" className="mt-5 flex flex-col bg-blue-800 w-70 h-120">
-        <div id="Progetti" className="mt-5 ml-5">
+      <div
+        id="generale"
+        className="mt-5 flex flex-col bg-[#0D5CC3] w-50 h-120 rounded-lg border border-white"
+      >
+        <div id="progetti" className="mt-5 pb-5 text-2xl text-center">
           Progetti
         </div>
-        <div id="Progetto 1" className="mt-2 ml-5">
+
+        <li id="progetto_1" className="mt-2 mr-5 text-lg text-center">
           Progetto 1
-        </div>
-        <div id="Progetto 2" className="mt-2 ml-5">
+        </li>
+        <li id="progetto_2" className="mt-2 mr-5 text-lg text-center">
           Progetto 2
-        </div>
+        </li>
       </div>
     </>
   );
--- portfolio/components/Riquadro_Progetto.js
+++ portfolio/components/Riquadro_Progetto.js
@@ -1,7 +1,7 @@
 export default function Progetto() {
   return (
     <>
-      <div className="mt-5 ml-10 bg-green-500 w-370 h-120">
+      <div className="mt-5 ml-10 bg-green-500 w-370 h-120 ">
         <div className="mt-5 ml-5">Progetto 1</div>
         <div className="mt-2 ml-5">Testo</div>
       </div>
