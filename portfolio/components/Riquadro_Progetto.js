9022ccba783c33c77cb76da82f454b2e78936b19
ErLupoDeRoma <l.rivoli@donboscosandona.it>
2025-05-19 17:12:37 +0200

    Cambio dello stile dell elemento Riquadro_Progetto

--- portfolio/components/Riquadro_Progetto.js
+++ portfolio/components/Riquadro_Progetto.js
@@ -1,9 +1,32 @@
 export default function Progetto() {
   return (
     <>
-      <div className="mt-5 ml-10 bg-green-500 w-370 h-120 ">
-        <div className="mt-5 ml-5">Progetto 1</div>
-        <div className="mt-2 ml-5">Testo</div>
+      <div className="mt-5 ml-10 bg-[#0D5CC3] w-370 h-120 rounded-lg border border-white flex justify-start">
+        <div className="w-5/5">
+          <div className="mt-5 ml-5 text-2xl font-semibold">Progetto 1</div>
+          <div className="mt-2 ml-5 text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            ultrices posuere nibh, nec dictum nisi lobortis ac. Donec aliquet,
+            tortor et pellentesque malesuada, ex lorem auctor turpis, non
+            sagittis mauris ante nec nulla. Aenean ut ipsum at orci laoreet
+            consectetur. Donec pulvinar dignissim finibus. Praesent pharetra
+            vulputate erat, a feugiat felis pharetra eget. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Mauris nulla arcu, bibendum non
+            lobortis vitae, viverra nec tellus. Curabitur fringilla, enim non
+            tristique molestie, augue nisl ultricies libero, at suscipit felis
+            lacus ut nibh. Nam placerat nisl a risus imperdiet, vitae mattis
+            justo pretium. Sed lorem tellus, sollicitudin eget ligula nec,
+            pulvinar accumsan libero. Sed accumsan fringilla eros, sed fermentum
+            erat porttitor ut.
+          </div>
+        </div>
+
+        <div className="m-5 flex justify-center items-center">
+          <img src="/images/Prodotto 1.png"
+          width="1800px"
+          height="600px" 
+          className="rounded-lg border border-white"/>
+        </div>
       </div>
     </>
   );
