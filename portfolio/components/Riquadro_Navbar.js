64ad3d603bf0f017e06f6e65b581674b1d3f2c77
ErLupoDeRoma <l.rivoli@donboscosandona.it>
2025-05-19 16:01:46 +0200

    Cambio dello stile dell elemento Riquadro_Navbar

--- portfolio/components/Riquadro_Navbar.js
+++ portfolio/components/Riquadro_Navbar.js
@@ -2,14 +2,14 @@ export default function Navbar() {
   return (
     <>
       
-      <div id="generale" className="mt-5 flex flex-row justify-around bg-blue-400 w-125 h-7">
-        <div id="chi sono" className="mt-1">
+      <div id="generale" className="mt-5 flex flex-row justify-around w-125 h-7">
+        <div id="chi_sono" className="px-6 rounded-lg transition nav-option">
           Chi sono
         </div>
-        <div id="esperienze" className="mt-1">
+        <div id="esperienze" className="px-6 rounded-lg transition nav-option">
           Esperienze
         </div>
-        <div id="progetti" className="mt-1">
+        <div id="progetti" className="px-6 rounded-lg transition nav-option">
           Progetti
         </div>
       </div>
