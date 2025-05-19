782fc6e627405d5cbfbf3e6f3f5e978ffca26dc9
ErLupoDeRoma <l.rivoli@donboscosandona.it>
2025-05-19 17:36:53 +0200

    Cambio dello stile dell elemento Riquadro_Immagini

--- portfolio/components/Riquadro_Immagini.js
+++ portfolio/components/Riquadro_Immagini.js
@@ -1,12 +1,12 @@
 export default function Immagini() {
   return (
     <>
-      <div id="generale" className="ml-125 flex bg-blue-800 w-120">
+      <div id="generale" className="ml-125 flex w-130">
         <div id="logo">
-          <img src="/images/Logo.webp" className="w-30 ml-40"></img>
+          <img src="/images/Logo.webp" className="w-30 ml-35 mr-5"></img>
         </div>
         <div id="foto">
-          <img src="/images/Persona.jpg" className="w-30 ml-75"></img>
+          <img src="/images/Persona quadrata.jpg" className="w-60 rounded-lg border border-white"></img>
         </div>
       </div>
     </>
