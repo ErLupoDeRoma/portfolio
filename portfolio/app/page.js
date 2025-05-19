9c98823a4f4a0424b0c4c0639e004125c9f1124e
ErLupoDeRoma <l.rivoli@donboscosandona.it>
2025-05-19 12:41:58 +0200

    Creato struttura Riquadro_Io

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
-
+import IO from "@/components/Riquadro_Io";
 
 export default function Home() {
   return (
    
+<IO/>
+
   );
 }
---
+++ portfolio/components/Riquadro_Io.js
@@ -0,0 +1,18 @@
+export default function IO() {
+  return (
+    <>
+      <div id="generale" className="flex flex-col bg-blue-500 w-125">
+        <div id="nome">
+            Luca Rivoli
+        </div>
+        <div id="info">
+          <ul>
+            <li>E-Mail=</li>
+            <li>Numero di telefono=</li>
+            <li>GitHub=</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
