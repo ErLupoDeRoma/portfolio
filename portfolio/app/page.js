933d31ffd2bc266a51e3c73d0d25f94e5bb9f996
ErLupoDeRoma <l.rivoli@donboscosandona.it>
2025-05-20 12:46:10 +0200

    Creato funzionalita Riquadri_Navbar

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,10 +1,20 @@
+"use client";
 import IO from "@/components/Riquadro_Io";
 import Immagini from "@/components/Riquadro_Immagini";
 import Navbar from "@/components/Riquadro_Navbar";
 import Progetti from "@/components/Sezione_progetti";
 import Chi_sono from "@/components/Sezione_Chi_sono";
+import { useState } from "react";
 
 export default function Home() {
+  const [sezione_da_mostrare, imposta_sezione] = useState("chi_sono");
+  const sezione_selezionata = () => {
+    if (sezione_da_mostrare == "chi_sono") {
+      return <Chi_sono />;
+    } else if (sezione_da_mostrare == "progetti") {
+      return <Progetti />;
+    }
+  };
   return (
     <div>
       <div className="mt-15 ml-15 flex justify-start ">
@@ -13,8 +23,8 @@ export default function Home() {
       </div>
       <br></br>
       <div className="grid grid-cols-1 mt-5 ml-15">
-        <Navbar />
-        <Chi_sono/>
+        <Navbar onSelect={imposta_sezione}/>
+        {sezione_selezionata()}
       </div>
     </div>
   );
--- portfolio/components/Riquadro_Navbar.js
+++ portfolio/components/Riquadro_Navbar.js
@@ -1,17 +1,17 @@
-export default function Navbar() {
+export default function Navbar({onSelect}) {
   return (
     <>
       
       <div id="generale" className="mt-5 flex flex-row justify-around w-125 h-7">
-        <div id="chi_sono" className="px-6 rounded-lg transition nav-option">
+        <button onClick={() => onSelect('chi_sono')} id="chi_sono" className="px-6 rounded-lg transition nav-option">
           Chi sono
-        </div>
-        <div id="esperienze" className="px-6 rounded-lg transition nav-option">
+        </button>
+        <button onClick={() => onSelect('esperienze')} id="chi_sono" className="px-6 rounded-lg transition nav-option">
           Esperienze
-        </div>
-        <div id="progetti" className="px-6 rounded-lg transition nav-option">
+        </button>
+        <button onClick={() => onSelect('progetti')} id="chi_sono" className="px-6 rounded-lg transition nav-option">
           Progetti
-        </div>
+        </button>
       </div>
     </>
   );
